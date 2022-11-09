5ff379e088fc31b0de0130e8f3139eb98a99602d
yangzhiwen777 <1599309671@qq.com>
2022-11-10 01:12:08 +0800

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import yayJpg from '../assets/yay.jpg';
 export default function HomePage() {
   return (
     <div>
-      <h2>Yay! Welcome to umi!22222222222222</h2>
+      <h2>Yay! Welcome to umi!3333</h2>
       <p>
         <img src={yayJpg} width="388" />
       </p>
